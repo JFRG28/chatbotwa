da22b7a40c3a4199f467c2c7ec03c50a5f19f39c
JFRG28 <fgrodriguez.28@gmail.com>
2026-05-06 18:21:50 -0600

    initial commit

---
+++ src/main/java/com/example/chatbot/wa/dev/chatbotwa/ChatbotwaApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatbot.wa.dev.chatbotwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatbot/wa/dev/chatbotwa/ChatbotwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatbot.wa.dev.chatbotwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
